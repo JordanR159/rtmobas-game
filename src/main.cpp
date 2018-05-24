43fd6c7caec0c2a5da8ba66c312220a1abab730b
JordanR159 <22157853+JordanR159@users.noreply.github.com>
2018-05-24 13:48:49 -0400

    Add code to test that SFML functions properly, add folder for fonts

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+    RenderWindow window(VideoMode(800, 600), "rtmobas-game");
+    RectangleShape shape;
+    shape.setSize(Vector2f(400, 100));
+    shape.setFillColor(Color::Blue);
+
+    Font font;
+    if(!font.loadFromFile("../fonts/BadhouseLight.ttf"))
+        return -1;
+
+    Text text("rtmobas-game", font, 80);
+    text.setFillColor(Color::Black);
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+
+            //Resizes drawing area of window, rescaling everything inside
+            if (event.type == Event::Resized)
+                window.setSize(Vector2u(event.size.width, event.size.height));
+
+            window.clear();
+            window.draw(shape);
+            window.draw(text);
+            window.display();
+        }
+    }
+
+    return 0;
+}
