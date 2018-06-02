71dafb339b290803b9a18107ac4c667c07877abd
Max O'Cull <maxattax97@gmail.com>
2018-06-01 20:02:42 -0400

    Update build script to handle itself

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ "$1" == "clean" ]]; then
+    rm -rf "build/" "rpmalloc/" "SFML/"
+    exit
+fi
+
+if [[ -n "$(command -v wget)" ]]; then
+    download_cmd="wget"
+elif [[ -n "$(command -v curl)" ]]; then
+    download_cmd="curl -O"
+fi
+
+$download_cmd "https://www.sfml-dev.org/files/SFML-2.5.0-linux-gcc-64-bit.tar.gz"
+tar -xzf "SFML-2.5.0-linux-gcc-64-bit.tar.gz" -C .
+rm -rf "SFML/"
+mv "SFML-2.5.0/" "SFML/"
+rm "SFML-2.5.0-linux-gcc-64-bit.tar.gz"
+
+git clone "https://github.com/rampantpixels/rpmalloc.git" "rpmalloc-temp/"
+rm -rf "rpmalloc/"
+mv "rpmalloc-temp/rpmalloc/" .
+rm -rf "rpmalloc-temp/"
+
+# There is an issue with RTLD_NEXT, Clang suggests compiling with RTLD_NOW (which works).
+sed -i 's/RTLD_NEXT/RTLD_NOW/g' "rpmalloc/malloc.c"
+
+rm -rf "build/"
+mkdir "build/"
+cmake -Bbuild/ -H.
+
+cd "build/" || exit
+make
